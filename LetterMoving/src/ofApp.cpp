e5560b60aced84e1abe17b2457106a139f94a0be
pierre tardif <pierre.tardif00@gmail.com>
2019-07-04 17:04:45 +0100

    wiggleText class

--- LetterMoving/src/ofApp.cpp
+++ LetterMoving/src/ofApp.cpp
@@ -4,22 +4,10 @@
 void ofApp::setup(){
     
     ofSetFrameRate(60);
-    ofSetWindowTitle("openFrameworks");
+    ofSetWindowTitle("wiggleText");
     
-    ofBackground(239);
-    ofSetLineWidth(3);
+    ofSetLineWidth(1);
     ofEnableDepthTest();
-    
-    this->font.loadFont("fonts/Cooper Black Regular.ttf", 300, true, true, true);
-    
-    
-    c = 'A';
-    
-    outline = outlineNoise = createVboLetter(c);
-    
-    outline = outlineNoise = createVboWord("wiggle");
-    
-    noise = vector < ofVec3f > (outline.getNumVertices());
     
     // colors
     top = 555930;
@@ -27,27 +15,22 @@ void ofApp::setup(){
     incTop = incBottom = 0.05;
     
     
-    //noise
-    indexLetter = 0;
-    freqNoise = 0.005;
+    wiggleText.setup();
+    wiggleText.setupMeshWord("Masino");
     
-    depthLetterMoves = 8.0;
-    depthNoise = 40.0;
-    speedNoise = 700.0;
-
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
-    updateNoise(noise.data());
-    addNoiseToLetter(&outline, &outlineNoise, noise.data());
     
     // gradiant colors
     top += incTop;
     if(top.r == 255)incTop = -incTop;
     bottom.g -= incBottom;
     if(bottom.g == 0)incBottom = -incBottom;
+    
+    // wiggle
+    wiggleText.update();
 }
 
 //--------------------------------------------------------------
@@ -55,17 +38,8 @@ void ofApp::draw(){
     
     ofBackgroundGradient(top, bottom, OF_GRADIENT_LINEAR);
     this->cam.begin();
-    ofRotateX(180);
     
-    
-    ofRotateY(ofGetElapsedTimef() * 30);
-    
-    ofPushMatrix();
-    ofTranslate(position.x, position.y);
-    outlineNoise.drawWireframe();
-//    path.draw(position.x, position.y);
-    
-    ofPopMatrix();
+    wiggleText.draw();
     
     this->cam.end();
 }
@@ -73,163 +47,16 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if(key ==  ' '){
-        pickRandomLetter();
+        wiggleText.pickRandomLetter();
     }
     if(key == OF_KEY_LEFT){
-        indexLetter -= 1;
-        pickLetter(indexLetter);
+        wiggleText.indexLetter -= 1;
+        wiggleText.pickLetter();
     }
     if(key == OF_KEY_RIGHT){
-        indexLetter += 1;
-        pickLetter(indexLetter);
+        wiggleText.indexLetter += 1;
+        wiggleText.pickLetter();
     }
-    outline = outlineNoise = createVboLetter(c);
-    noise = vector < ofVec3f > (outline.getNumVertices());
+    wiggleText.setupMeshLetter(wiggleText.c);
 
-}
-void ofApp::pickLetter(int indexLetter){
-    c = (char)(indexLetter + 65);
-}
-
-void ofApp::pickRandomLetter(){
-    int n = (int)(ofRandom(26));
-    c = (char)(n + 65);
-    
-    indexLetter = n;
-}
-
-
-ofVboMesh ofApp::createVboLetter(char c){
-    ofPath path = font.getCharacterAsPoints(c, true, false);
-    
-    position = {-font.getStringBoundingBox(ofToString(c), 0,0).width / 2, font.getStringBoundingBox(ofToString(c), 0,0).height / 2};
-    
-    
-    vector < ofVec3f > points;
-    int indexVertex = 0;
-    
-    for(int i = 0; i < path.getOutline().size(); i++){
-        ofPolyline p = path.getOutline()[i];
-        size_t l = p.getVertices().size();
-        
-        bool even = true;
-        for(int j = 0; j < l; j++){
-            
-            ofVec3f vec = p.getVertices()[j];
-            addDepth(&vec, indexVertex);
-            points.push_back(vec);
-            if(j+1 < l){
-                
-                ofVec3f vec2 = p.getVertices()[j+1];
-                addDepth(&vec2, indexVertex + 1);
-                vector< ofVec3f > p = addToVector( 10, vec, vec2);
-                for(int k = 0;k < p.size();k++){
-                    points.push_back(p[k]);
-                }
-                
-                points.push_back(vec2);
-                
-            }
-            if(j == l-1){
-                points.push_back(p.getVertices()[0]);
-            }
-            indexVertex ++;
-        }
-    }
-    
-    ofVboMesh line;
-    line.setMode(OF_PRIMITIVE_LINES);
-    for( int i = 0; i < points.size(); i++){
-        line.addVertex(points[i]);
-    }
-    
-    
-    return line;
-}
-
-//--------------------------------------------------------------
-void ofApp::addDepth(ofVec3f* vec, int index){
-    (*vec).z = (index % 5) * depthLetterMoves;
-    
-}
-
-
-//--------------------------------------------------------------
-void ofApp::updateNoise(ofVec3f* noisePointer ){
-    for( int i = 0; i < noise.size(); i++){
-        (*noisePointer).z = depthNoise * ofNoise((i + ofGetElapsedTimef() * speedNoise ) * freqNoise);
-        *(noisePointer++);
-    }
-}
-
-
-//--------------------------------------------------------------
-void ofApp::addNoiseToLetter(ofVboMesh* vLetters, ofVboMesh* vNoise, ofVec3f* noisePointer){
-    for(int i = 0; i < vLetters->getNumVertices(); i++){
-        ofVec3f pos = vLetters->getVertex(i);
-        pos.z  += (*noisePointer).z;
-        vNoise->setVertex(i, pos);
-        if(i%2 == 1){
-            pos = vLetters->getVertex(i);
-            pos.z  += ( *( noisePointer+1) ).z;
-            vNoise->setVertex(i, pos);
-            
-        }
-        if(i == vLetters->getNumVertices() - 1){
-            pos = vLetters->getVertex(i);
-            pos.z  += (noise[0]).z;
-            vNoise->setVertex(i, pos);
-        }
-        *(noisePointer++);
-    }
-}
-
-vector < ofVec3f> ofApp::addToVector(int amount, ofVec3f p2, ofVec3f p1){
-    vector < ofVec3f > v;
-    for(int i = 1; i < amount * 2 - 1; i++){
-        float fractionVal = (float)(i) / (float)(2 * amount);
-        ofVec3f p3 = {  static_cast<float>(p1.x * fractionVal  + p2.x * (1 - fractionVal)),
-            static_cast<float>(p1.y * fractionVal  + p2.y * (1 - fractionVal)),
-            static_cast<float>(p1.z * fractionVal  + p2.z * (1 - fractionVal))};
-        v.push_back(p3);
-        v.push_back(p3);
-    }
-    return v;
-}
-
-ofVec2f ofApp::positionLetter(char c){
-    ofPath path = font.getCharacterAsPoints(c, true, false);
-    
-    return {font.getStringBoundingBox(ofToString(c), 0,0).width, font.getStringBoundingBox(ofToString(c), 0,0).height};
-}
-
-
-ofVboMesh ofApp::createVboWord(string word){
-    
-    ofVboMesh vbo;
-    vbo.setMode(OF_PRIMITIVE_LINES);
-    
-    ofVec2f positionCurrLetter = {0,0};
-    
-    ofVboMesh tempVbo;
-    for( int i = 0; i < word.length(); i++){
-        char c = word[i];
-        cout << "c = " << c << endl;
-        tempVbo = createVboLetter(c);
-        ofVec2f posLetter = positionLetter(c);
-        
-        for( int j = 0; j < tempVbo.getNumVertices(); j++){
-            ofVec3f pVert = tempVbo.getVertex(j);
-            pVert.x += positionCurrLetter.x;
-            vbo.addVertex(pVert);
-            
-        }
-        positionCurrLetter.x += posLetter.x;
-        if(i == 0)positionCurrLetter.y += posLetter.y;
-        
-    }
-    
-    position = {-positionCurrLetter.x / 2, positionCurrLetter.y / 4};
-    
-    return vbo;
 }
--- LetterMoving/src/ofApp.h
+++ LetterMoving/src/ofApp.h
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+#include "WiggleText/WiggleText.hpp"
+
 class ofApp : public ofBaseApp{
 	public:
 		void setup();
@@ -12,35 +14,17 @@ class ofApp : public ofBaseApp{
 		
 		void keyPressed(int key);
     
+    //background colors
     ofColor top, bottom;
     float incTop, incBottom;
     
-    ofTrueTypeFont font;
     ofEasyCam cam;
     
-    
-    char c;
-    int indexLetter;
-    ofVboMesh outline, outlineNoise;
-    ofVec2f position;
-    
-    ofVec2f positionLetter(char c);
-    ofVboMesh createVboLetter(char c);
-    ofVboMesh createVboWord(string word);
-    vector < ofVec3f> addToVector( int amount, ofVec3f p1, ofVec3f p2);
-    float depthLetterMoves, depthNoise, speedNoise;
-    float freqNoise;
-    void addDepth(ofVec3f* vec, int index);
+    WiggleText wiggleText;
 
     
-    void pickRandomLetter();
-    void pickLetter(int indexLetter);
     
     
-    // noise
-    vector < ofVec3f > noise;
-    void addNoiseToLetter(ofVboMesh* vLetters, ofVboMesh* vNoise, ofVec3f* noise);
-    void updateNoise(ofVec3f* noisePointer );
     
     
 };
